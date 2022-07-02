0b3855a3d5d0108e47e5f044f141b014a2a62f16
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2022-07-02 05:20:23 -0300

    att

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,11 +5,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-          <meta property="og:url" content="URL" />
-          <meta property="og:title" content="TÍTULO" />
-          <meta property="og:image" content="URL IMAGEM" />
-          <meta property="og:description" content="DESCRIÇÃO" />
-          <meta name="theme-color" content="#fe2813"/>
         </Head>
       <body>
         <Main />
